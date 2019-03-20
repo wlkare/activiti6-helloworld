cd60cb7dd15d94d1a8d3b2df16a10e564fba20e8
wang <13667376107@139.com>
2019-03-20 17:26:51 +0800

    liucheng

--- src/main/java/com/wang/activiti/helloworld/DemoMain.java
+++ src/main/java/com/wang/activiti/helloworld/DemoMain.java
@@ -1,9 +1,15 @@
 package com.wang.activiti.helloworld;
 
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.*;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentBuilder;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * 启动类
@@ -18,17 +24,49 @@ public class DemoMain {
         LOGGER.info("启动我们的程序");
 
         //创建流程引擎
+        ProcessEngine processEngine = getProcessEngine();
+
+        //部署流程定义文件
+        ProcessDefinition processDefinition = getProcessDefinition(processEngine);
+
+
+
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
+        LOGGER.info("启动流程 [{}]", processInstance.getProcessDefinitionKey());
+
+
+        //处理流程任务
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> list = taskService.createTaskQuery().list();
+        for (Task task : list){
+            LOGGER.info("待处理任务 [{}]",task.getName());
+        }
+        LOGGER.info("待处理任务数量 [{}]",list.size());
+
+        LOGGER.info("结束我们的程序");
+    }
+
+    private static ProcessDefinition getProcessDefinition(ProcessEngine processEngine) {
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        DeploymentBuilder deploymentBuilder = repositoryService.createDeployment();
+        deploymentBuilder.addClasspathResource("second_approve.bpmn20.xml");
+        Deployment deployment = deploymentBuilder.deploy();
+        String deploymentId = deployment.getId();
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+                .deploymentId(deploymentId)
+                .singleResult();
+        LOGGER.info("流程定义文件 [{}], 流程ID [{}]", processDefinition.getName(), processDefinition.getId());
+        return processDefinition;
+    }
+
+    private static ProcessEngine getProcessEngine() {
         ProcessEngineConfiguration cfg = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration();
         ProcessEngine processEngine = cfg.buildProcessEngine();
         String name = processEngine.getName();
         String version = ProcessEngine.VERSION;
 
-        LOGGER.info("流程引擎名称{},版本{}",name,version);
-        //部署流程定义文件
-
-        //启动运行流程
-
-        //处理流程任务
-        LOGGER.info("结束我们的程序");
+        LOGGER.info("流程引擎名称[{}],版本[{}]",name,version);
+        return processEngine;
     }
 }
