e18506485d2801810158c65be6c80fae0e306315
wlkare <13667376107@139.com>
2019-03-20 22:41:19 +0800

    完成二级审批流程

--- src/main/java/com/wang/activiti/helloworld/DemoMain.java
+++ src/main/java/com/wang/activiti/helloworld/DemoMain.java
@@ -1,6 +1,11 @@
 package com.wang.activiti.helloworld;
 
+import com.google.common.collect.Maps;
 import org.activiti.engine.*;
+import org.activiti.engine.form.FormProperty;
+import org.activiti.engine.form.TaskFormData;
+import org.activiti.engine.impl.form.DateFormType;
+import org.activiti.engine.impl.form.StringFormType;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.engine.repository.ProcessDefinition;
@@ -9,7 +14,12 @@ import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * 启动类
@@ -19,7 +29,7 @@ public class DemoMain {
     private static final Logger LOGGER = LoggerFactory.getLogger(DemoMain.class);
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         LOGGER.info("启动我们的程序");
 
@@ -29,22 +39,63 @@ public class DemoMain {
         //部署流程定义文件
         ProcessDefinition processDefinition = getProcessDefinition(processEngine);
 
-
-
-        RuntimeService runtimeService = processEngine.getRuntimeService();
-        ProcessInstance processInstance = runtimeService.startProcessInstanceById(processDefinition.getId());
+        //启动运行流程
+        ProcessInstance processInstance = getProcessInstance(processEngine, processDefinition);
         LOGGER.info("启动流程 [{}]", processInstance.getProcessDefinitionKey());
 
-
         //处理流程任务
-        TaskService taskService = processEngine.getTaskService();
-        List<Task> list = taskService.createTaskQuery().list();
-        for (Task task : list){
-            LOGGER.info("待处理任务 [{}]",task.getName());
-        }
-        LOGGER.info("待处理任务数量 [{}]",list.size());
-
+        processTask(processEngine, processInstance);
         LOGGER.info("结束我们的程序");
+    }
+
+    private static void processTask(ProcessEngine processEngine, ProcessInstance processInstance) throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        while(processInstance!=null && !processInstance.isEnded()){
+            TaskService taskService = processEngine.getTaskService();
+            List<Task> list = taskService.createTaskQuery().list();
+            LOGGER.info("待处理任务数量 [{}]",list.size());
+            for (Task task : list){
+                LOGGER.info("待处理任务 [{}]",task.getName());
+
+                Map<String, Object> variables = getMap(processEngine, scanner, task);
+                taskService.complete(task.getId(), variables);
+                processInstance = processEngine.getRuntimeService()
+                        .createProcessInstanceQuery()
+                        .processInstanceId(processInstance.getId())
+                        .singleResult();
+            }
+        }
+        scanner.close();
+    }
+
+    private static Map<String, Object> getMap(ProcessEngine processEngine, Scanner scanner, Task task) throws ParseException {
+        Map<String, Object> variables = Maps.newHashMap();
+        FormService formService = processEngine.getFormService();
+        TaskFormData taskFormData = formService.getTaskFormData(task.getId());
+        List<FormProperty> formProperties = taskFormData.getFormProperties();
+        for (FormProperty formProperty : formProperties) {
+            String line = null;
+            if (StringFormType.class.isInstance(formProperty.getType())){
+                LOGGER.info("请输入 {} ？",formProperty.getName());
+                line = scanner.nextLine();
+                variables.put(formProperty.getId(), line);
+            }else if (DateFormType.class.isInstance(formProperty.getType())){
+                LOGGER.info("请输入 {} ？ 格式 （yyyy-MM-dd）",formProperty.getName());
+                line = scanner.nextLine();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = dateFormat.parse(line);
+                variables.put(formProperty.getId(), date);
+            }else{
+                LOGGER.info("类型暂不支持 {}",formProperty.getType());
+            }
+            LOGGER.info("您输入的内容是 [{}]",line);
+        }
+        return variables;
+    }
+
+    private static ProcessInstance getProcessInstance(ProcessEngine processEngine, ProcessDefinition processDefinition) {
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        return runtimeService.startProcessInstanceById(processDefinition.getId());
     }
 
     private static ProcessDefinition getProcessDefinition(ProcessEngine processEngine) {
